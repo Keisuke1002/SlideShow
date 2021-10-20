31f0a4d3a919c642cd02b0ab5814f4ed15ea7fa0
Keisuke1002 <92850608+Keisuke1002@users.noreply.github.com>
2021-10-20 17:26:06 +0900

    Add files via upload

---
+++ js/slideShowV3.js
@@ -0,0 +1,73 @@
+let count = 1;
+let nowPlaying=false;
+let timer;
+
+const src="https://www.takushoku-u.ac.jp/summary/images/summary_successive-chancellor_img_"
+let mainElement = document.querySelector("div.main>img");
+let thumbnailsElement = document.querySelector("div.thumbnails");
+const MIN=1;
+const MAX=19;
+
+function left(){
+  count = count -  1;
+  let url;
+  if(count<MIN){
+    count = MAX;
+    thumbnailsElement.children[0].classList.remove("selected");
+  }else {
+    thumbnailsElement.children[count].classList.remove("selected");
+  }
+  if(count<10){
+    url =src+"0"+count+".jpg";
+  }else{
+    url =src+count+".jpg";
+  }
+  mainElement.setAttribute('src',url);
+  thumbnailsElement.children[count-1].classList.add("selected");
+}
+
+function right(){
+  count = count +  1;
+  let url;
+  if(count>MAX){
+    count = MIN;
+    thumbnailsElement.children[MAX-1].classList.remove("selected");
+  }else {
+    thumbnailsElement.children[count-2].classList.remove("selected");
+  }
+  if(count<10){
+    url =src+"0"+count+".jpg";
+  }else{
+    url =src+count+".jpg";
+  }
+  mainElement.setAttribute('src',url);
+  thumbnailsElement.children[count-1].classList.add("selected");
+}
+
+function autoPlay(){
+  timer = setTimeout(function(){
+    right()
+  autoPlay()
+}, 1000);
+}
+
+function play(){
+  if(!nowPlaying){
+    autoPlay()
+  }
+}
+
+function stop(){
+  clearTimeout(timer);
+  nowPlaying=false;
+}
+
+function reset(){
+  stop();
+  console.log(count);
+  thumbnailsElement.children[count-1].classList.remove("selected");
+  thumbnailsElement.children[0].classList.add("selected");
+  url="https://www.takushoku-u.ac.jp/summary/images/summary_successive-chancellor_img_01.jpg";
+  mainElement.setAttribute('src',url);
+  count=1;
+}
